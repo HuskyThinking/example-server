5ebeb040a7a7fc4d2ee22a7d9c2a27231f8d5647
LIDX <849627763@qq.com>
2024-02-29 21:01:44 +0800

    建立db和mq

---
+++ server-mq/src/main/java/com/husky/servermq/ServerMqApplication.java
@@ -0,0 +1,13 @@
+package com.husky.servermq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerMqApplication.class, args);
+    }
+
+}
---
+++ server-mq/src/test/java/com/husky/servermq/ServerMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.husky.servermq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
