fe6c0775d627d46397868ea74ffdc47baf79268c
LIDX <849627763@qq.com>
2024-03-01 22:03:59 +0800

    redis-example

--- server-db/src/test/java/com/husky/serverdb/sdredis/SpringDataRedisTest.java
+++ server-db/src/test/java/com/husky/serverdb/sdredis/SpringDataRedisTest.java
@@ -1,9 +1,13 @@
 package com.husky.serverdb.sdredis;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.husky.serverdb.redis.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
 /**
@@ -15,8 +19,13 @@ import org.springframework.data.redis.core.ValueOperations;
 @SpringBootTest
 public class SpringDataRedisTest {
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Test
     void testSpring() {
@@ -30,4 +39,31 @@ public class SpringDataRedisTest {
         System.out.println("blogName = " + blogName);
     }
 
+    @Test
+    void testSaveUser() {
+        // 写入数据
+        redisTemplate.opsForValue().set("user:100",new User("LIDX",30));
+        redisTemplate.opsForValue().set("user:90",new User("husky",60));
+
+        // 获取数据
+        User user = (User) redisTemplate.opsForValue().get("user:90");
+
+        System.out.println(user);
+    }
+
+    @Test
+    void testSaveUserTwo() throws JsonProcessingException {
+        // 2.将要存入的对象通过Json序列化对象转换为字符串
+        String userJson1 = objectMapper.writeValueAsString(new User("Vz", 21));
+        // 3.通过StringRedisTemplate将数据存入redis
+        stringRedisTemplate.opsForValue().set("user:100",userJson1);
+        // 4.通过key取出value
+        String userJson2 = stringRedisTemplate.opsForValue().get("user:100");
+        // 5.由于取出的值是String类型的Json字符串，因此我们需要通过Json序列化对象来转换为java对象
+        User user = objectMapper.readValue(userJson2, User.class);
+        // 6.打印结果
+        System.out.println("user = " + user);
+    }
+
 }
+
