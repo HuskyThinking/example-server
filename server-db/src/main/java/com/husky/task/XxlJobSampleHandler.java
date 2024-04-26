e644eeba233f9090e098273e381008880bb39a4a
LIDX <849627763@qq.com>
2024-04-26 10:56:22 +0800

    生成日志配置

--- server-db/src/main/java/com/husky/task/XxlJobSampleHandler.java
+++ server-db/src/main/java/com/husky/task/XxlJobSampleHandler.java
@@ -1,5 +1,6 @@
 package com.husky.task;
 
+import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,8 @@ public class XxlJobSampleHandler {
 
     @XxlJob("demoJobHandler")
     public void demoJobHandler() {
+        String param = XxlJobHelper.getJobParam();
+        log.info("接收調度中心参数...[{}]", param);
         log.info("XXL-JOB, Hello World.");
     }
 
